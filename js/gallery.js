a14506c1c5ddcf5fad06e2390aa1751fb246e265
Jason Ching <222jching@frhsd.com>
2022-02-09 11:13:29 -0500

    js

--- js/gallery.js
+++ js/gallery.js
@@ -1,28 +1,35 @@
 // requestAnim shim layer by Paul Irish
-    window.requestAnimFrame = (function(){
-      return  window.requestAnimationFrame       ||
-              window.webkitRequestAnimationFrame ||
-              window.mozRequestAnimationFrame    ||
-              window.oRequestAnimationFrame      ||
-              window.msRequestAnimationFrame     ||
-              function(/* function */ callback, /* DOMElement */ element){
-                window.setTimeout(callback, 1000 / 60);
-              };
-    })();
+
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          window.oRequestAnimationFrame      ||
+          window.msRequestAnimationFrame     ||
+          function(/* function */ callback, /* DOMElement */ element){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
 
 
 // example code from mr doob : http://mrdoob.com/lab/javascript/requestanimationframe/
 
+
+
 animate();
+
+
 
 var mLastFrameTime = 0;
 var mWaitTime = 5000; //time in ms
 function animate() {
-    requestAnimFrame( animate );
+   requestAnimFrame( animate );
 	var currentTime = new Date().getTime();
 	if (mLastFrameTime === 0) {
 		mLastFrameTime = currentTime;
 	}
+
+
 
 	if ((currentTime - mLastFrameTime) > mWaitTime) {
 		swapPhoto();
@@ -30,26 +37,36 @@ function animate() {
 	}
 }
 
+
+
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
 
 function swapPhoto() {
 
+
+
 if(mCurrentIndex >= mImages.length)
 {
-  mCurrentIndex = 0;
+ mCurrentIndex = 0;
 }
+
+
 
 if(mCurrentIndex < 0) {
-  mCurrentIndex = mImages.length-1;
+ mCurrentIndex = mImages.length-1;
 }
 
-  document.getElementById('photo').src = mImages[mCurrentIndex].img;
-  var loc = document.getElementsByClassName('location');
-  loc[0].innerHTML = "location: " + mImages[mCurrentIndex].location;
-  var des = document.getElementsByClassName('description');
-  des[0].innerHTML = "Description: " + mImages[mCurrentIndex].description;
-  var dt = document.getElementsByClassName('date');
-  dt[0].innerHTML = "Date: " + mImages[mCurrentIndex].date;
+
+
+ document.getElementById('photo').src = mImages[mCurrentIndex].img;
+ var loc = document.getElementsByClassName('location');
+ loc[0].innerHTML = "location: " + mImages[mCurrentIndex].location;
+ var des = document.getElementsByClassName('description');
+ des[0].innerHTML = "Description: " + mImages[mCurrentIndex].description;
+ var dt = document.getElementsByClassName('date');
+ dt[0].innerHTML = "Date: " + mImages[mCurrentIndex].date;
+
+
 
 mLastFrameTime = 0;
 mCurrentIndex += 1;
@@ -57,28 +74,37 @@ mCurrentIndex += 1;
 
 function toggleDetails()
 {
-  if($(" .moreIndicator").hasClass("rot90"))
-  {
-    $( " .moreIndicator").removeClass("rot90");
-    $(".moreIndicator").addClass("rot270");
+ if($(" .moreIndicator").hasClass("rot90"))
+ {
+   $( " .moreIndicator").removeClass("rot90");
+   $(".moreIndicator").addClass("rot270");
 }
-  else {
-    $( " .moreIndicator").removeClass("rot270");
-    $(".moreIndicator").addClass("rot90");
+ else {
+   $( " .moreIndicator").removeClass("rot270");
+   $(".moreIndicator").addClass("rot90");
 }
-  $( ".details" ).slideToggle( "slow", "linear" );
+ $( ".details" ).slideToggle( "slow", "linear" );
 }
+
+
+
 
 
 
 // Counter for the mImages array
 var mCurrentIndex = 0;
 
+
+
 // XMLHttpRequest variable
 var mRequest = new XMLHttpRequest();
 
+
+
 // Array holding GalleryImage objects (see below).
 var mImages = [];
+
+
 
 // Holds the retrived JSON information
 var mJson;
@@ -87,28 +113,30 @@ var mJson;
 var mUrl = 'images.json';
 function fetchJSON()
 {
-  mRequest.onreadystatechange = function() {
-  console.log("on ready state change");
-  if(this.readyState == 4 && this.status == 200) {
-      mJson = JSON.parse(mRequest.responseText);
-      iterateJSON(mJson);
-    }
-  }
-  mRequest.open("GET", mUrl, true);
-  mRequest.send();
+ mRequest.onreadystatechange = function() {
+ console.log("on ready state change");
+ if(this.readyState == 4 && this.status == 200) {
+     mJson = JSON.parse(mRequest.responseText);
+     iterateJSON(mJson);
+   }
+ }
+ mRequest.open("GET", mUrl, true);
+ mRequest.send();
 }
+
+
 
 function iterateJSON(mJson)
 {
-  for( x = 0; x < mJson.images.length; x++ )
-    {
-      mImages[x] = new GalleryImage();
-      mImages[x].location = mJson.images[x].imgLocation;
-      mImages[x].description = mJson.images[x].description;
-      mImages[x].date = mJson.images[x].date;
-      mImages[x].img = mJson.images[x].imgPath;
-    }
-  }
+ for( x = 0; x < mJson.images.length; x++ )
+   {
+     mImages[x] = new GalleryImage();
+     mImages[x].location = mJson.images[x].imgLocation;
+     mImages[x].description = mJson.images[x].description;
+     mImages[x].date = mJson.images[x].date;
+     mImages[x].img = mJson.images[x].imgPath;
+   }
+ }
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
 function makeGalleryImageOnloadCallback(galleryImage) {
@@ -120,11 +148,17 @@ function makeGalleryImageOnloadCallback(galleryImage) {
 
 
 
+
+
+
+
+
+
 $(document).ready( function() {
 
 	// This initially hides the photos' metadata information
 	//$('.details').eq(0).hide();
-  $( "#nextPhoto" ).position({
+ $( "#nextPhoto" ).position({
 my: "right bottom",
 at: "right bottom",
 of: "#nav"
@@ -133,21 +167,20 @@ of: "#nav"
 const urlParams = new URLSearchParams(window.location.search);
 
 for (const [key, value] of urlParams) {
-  console.log(`${key}:${value}`);
-  mUrl = value;
+ console.log(`${key}:${value}`);
+ mUrl = value;
 }
 if(mUrl == undefined)
 {
-  mUrl = 'images.json';
+ mUrl = 'images.json';
 }
+
+
 
 fetchJSON();
 
-
-
-
-
 });
+
 
 window.addEventListener('load', function() {
 
@@ -157,14 +190,13 @@ window.addEventListener('load', function() {
 
 function GalleryImage() {
 
+ var location;
 
-  var location;
+ var description;
 
-  var description;
+ var date;
 
-  var date;
-
-  var img;
+ var img;
 	//implement me as an object to hold the following data about an image:
 	//1. location where photo was taken
 	//2. description of photo
